4aae17f6f8841b2ce292e1ea168b41cbb6207f5e
tracer-ics2016 <tracer@njuics.org>
2020-10-19 11:01:38 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:01:38 up 6 days,  7:06,  1 user,  load average: 0.79, 0.38, 0.15 261adda6a68f32aa3f1857a980addd5d97ecdd57

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,9 +1,48 @@
 #include "common.h"
+#include <stdlib.h>
+#include "burst.h"
+#include "cpu/reg.h"
+#define BLOCK_SIZE 64
+#define STORAGE_SIZE_L1 64*1024
+#define STORAGE_SIZE_L2 4*1024*1024
+#define EIGHT_WAY 8
+#define SIXTEEN_WAY 16
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
+
+struct Cache{
+	bool tag;
+	bool valid;
+	uint8_t data[BLOCK_SIZE];
+}cache[STORAGE_SIZE_L1/BLOCK_SIZE];
+struct Cache2{
+	bool tag;
+	bool valid,dirty;
+	uint8_t data[BLOCK_SIZE];
+}cache2[STORAGE_SIZE_L2/BLOCK_SIZE];
+
+void initCache(){
+	int i;
+	for(i=0;i<STORAGE_SIZE_L1/BLOCK_SIZE;i++){
+		cache[i].tag=false;
+		cache[i].valid=false;
+		memset (cache[i].data,0,BLOCK_SIZE);
+	}
+	for(i=0;i<STORAGE_SIZE_L2/BLOCK_SIZE;i++){
+		cache2[i].tag=false;
+		cache2[i].valid=false;
+		cache2[i].dirty=false;
+		memset (cache2[i].data,0,BLOCK_SIZE);
+	}
+
+}
+
+uint32_t cache_read(hwaddr_t addr, size_t len){
+	return 0;
+}
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
