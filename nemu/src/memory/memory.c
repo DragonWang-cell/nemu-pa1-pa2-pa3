11611f3dd03fee9b8ea77e018e42cf57033f97cd
tracer-ics2016 <tracer@njuics.org>
2020-10-20 10:37:14 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:37:14 up 7 days,  6:42,  1 user,  load average: 0.71, 0.89, 0.61 45ac09a163642b3e7bc08e17fb3107057c81220c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -102,11 +102,24 @@ void cache2_write(hwaddr_t addr,size_t len,uint32_t data){
 }
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int id = cache_read(addr);
+	uint32_t offset = addr&(CACHE_BLOCK_SIZE-1);
+	uint8_t tmp[2*BURST_LEN];
+	if(offset+len>CACHE_BLOCK_SIZE) {
+		int id2 = cache_read(addr+CACHE_BLOCK_SIZE-offset);	
+		memcpy(tmp,cache[id].data+offset,CACHE_BLOCK_SIZE-offset);
+		memcpy(tmp+CACHE_BLOCK_SIZE-offset,cache[id2].data,len-(CACHE_BLOCK_SIZE-offset));
+	}
+	else {
+		memcpy(tmp,cache[id].data+offset,len);
+	}
+	int zero=0;
+	uint32_t ans = unalign_rw(tmp+zero,4)&(~0u >> ((4 - len) << 3));
+	return ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
+	cache_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
