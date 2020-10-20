6d1a6302f8cf2dc0258ce18b2b89c2d5c30dd19d
tracer-ics2016 <tracer@njuics.org>
2020-10-20 10:31:22 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:31:22 up 7 days,  6:36,  1 user,  load average: 0.38, 0.34, 0.27 1c5320d41f8420b217ee26f1f352cf2fd43dcf

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,47 +1,104 @@
 #include "common.h"
 #include <stdlib.h>
 #include "burst.h"
-#include "cpu/reg.h"
-#define BLOCK_SIZE 64
-#define STORAGE_SIZE_L1 64*1024
-#define STORAGE_SIZE_L2 4*1024*1024
-#define EIGHT_WAY 8
-#define SIXTEEN_WAY 16
+#include "memory.h"
+#define CACHE_BLOCK_SIZE_B 6
+#define CACHE_WAY_SIZE_B 3
+#define CACHE_SET_SIZE_B 7
+#define CACHE_BLOCK_SIZE (1<<CACHE_BLOCK_SIZE_B)
+#define CACHE_WAY_SIZE (1<<CACHE_WAY_SIZE_B)
+#define CACHE_SET_SIZE (1<<CACHE_SET_SIZE_B)
+
+#define CACHE2_BLOCK_SIZE_B 6
+#define CACHE2_WAY_SIZE_B 4
+#define CACHE2_SET_SIZE_B 12
+#define CACHE2_BLOCK_SIZE (1<<CACHE2_BLOCK_SIZE_B)
+#define CACHE2_WAY_SIZE (1<<CACHE2_WAY_SIZE_B)
+#define CACHE2_SET_SIZE (1<<CACHE2_SET_SIZE_B)
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
-
-/* Memory accessing interfaces */
-
+void cache_write(hwaddr_t addr,size_t len,uint32_t data);
+void cache2_write(hwaddr_t addr,size_t len,uint32_t data);
+uint32_t cache_read(hwaddr_t addr);
+uint32_t cache2_read(hwaddr_t addr);
+// jie gou ti
 struct Cache{
-	bool tag;
+	uint32_t tag;
 	bool valid;
-	uint8_t data[BLOCK_SIZE];
-}cache[STORAGE_SIZE_L1/BLOCK_SIZE];
+	uint8_t data[CACHE_BLOCK_SIZE];
+}cache[CACHE_WAY_SIZE*CACHE_SET_SIZE];
 struct Cache2{
-	bool tag;
+	uint32_t tag;
 	bool valid,dirty;
-	uint8_t data[BLOCK_SIZE];
-}cache2[STORAGE_SIZE_L2/BLOCK_SIZE];
+	uint8_t data[CACHE2_BLOCK_SIZE];
+}cache2[CACHE2_WAY_SIZE*CACHE2_SET_SIZE];
 
 void initCache(){
 	int i;
-	for(i=0;i<STORAGE_SIZE_L1/BLOCK_SIZE;i++){
-		cache[i].tag=false;
+	for(i=0;i<CACHE_WAY_SIZE*CACHE_SET_SIZE;i++){
+		cache[i].tag=0;
 		cache[i].valid=false;
-		memset (cache[i].data,0,BLOCK_SIZE);
+		memset (cache[i].data,0,CACHE_BLOCK_SIZE);
 	}
-	for(i=0;i<STORAGE_SIZE_L2/BLOCK_SIZE;i++){
-		cache2[i].tag=false;
+	for(i=0;i<CACHE2_WAY_SIZE*CACHE2_SET_SIZE;i++){
+		cache2[i].tag=0;
 		cache2[i].valid=false;
 		cache2[i].dirty=false;
-		memset (cache2[i].data,0,BLOCK_SIZE);
+		memset (cache2[i].data,0,CACHE2_BLOCK_SIZE);
 	}
-
 }
 
-uint32_t cache_read(hwaddr_t addr, size_t len){
+uint32_t cache_read(hwaddr_t addr){
+	uint32_t flag = addr>>(CACHE_SET_SIZE_B+CACHE_BLOCK_SIZE_B);
+	uint32_t set = addr>>(CACHE_BLOCK_SIZE_B);
+	set &=(CACHE_SET_SIZE-1);
+	int i;
+	for(i=set*CACHE_WAY_SIZE;i<(set+1)*CACHE_WAY_SIZE;i++) {
+		if(cache[i].valid&&cache[i].tag==flag) {
+			return i;
+		}
+	}
+	int j = cache2_read(addr);
+	srand(i);
+	i = CACHE_WAY_SIZE *set + rand()%CACHE_WAY_SIZE;
+	memcpy(cache[i].data,cache2[j].data,CACHE_BLOCK_SIZE);
+	cache[i].valid = true;
+	cache[i].tag = flag;
+	return i;
+}
+
+void cache_write(hwaddr_t addr,size_t len,uint32_t data){
+	uint32_t tag = addr>>(CACHE_SET_SIZE_B+CACHE_BLOCK_SIZE_B);
+	uint32_t set = addr>>(CACHE_BLOCK_SIZE_B);
+	set &=(CACHE_SET_SIZE-1);
+	uint32_t offset = addr&(CACHE_BLOCK_SIZE-1);
+	
+	int i;
+	for(i=set*CACHE_WAY_SIZE;i<(set+1)*CACHE_WAY_SIZE;i++) {
+		if(cache[i].valid&&cache[i].tag==tag) {
+			if(offset+len>CACHE_BLOCK_SIZE) {
+				memcpy(cache[i].data+offset,&data,CACHE_BLOCK_SIZE-offset);
+				cache2_write(addr,CACHE_BLOCK_SIZE-offset,data);
+				cache_write(addr+CACHE_BLOCK_SIZE-offset,len-(CACHE_BLOCK_SIZE-offset),data>>(CACHE_BLOCK_SIZE-offset));
+			}	
+			else {	
+				memcpy(cache[i].data+offset,&data,len);	
+				cache2_write(addr,len,data);		
+			}
+			return ;
+		}
+	}
+	cache2_write(addr,len,data);	
+}
+
+uint32_t cache2_read(hwaddr_t addr){
 	return 0;
+
+}
+void cache2_write(hwaddr_t addr,size_t len,uint32_t data){
+	return ;
+
 }
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
