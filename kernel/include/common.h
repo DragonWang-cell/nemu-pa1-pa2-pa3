68c86062b34c0f92aef54ef2f8bd519ba390c38b
tracer-ics2016 <tracer@njuics.org>
2020-10-21 01:54:51 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:54:51 up 7 days, 21:59,  1 user,  load average: 0.81, 0.42, 0.19 aeecae8110f445686c2a8a3eb3553214d690cba

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -6,6 +6,8 @@
 //#define IA32_PAGE
 //#define IA32_INTR
 //#define HAS_DEVICE
+#define IA32_SEG
+#define IA32_PAGE
 
 #ifndef __ASSEMBLER__
 /* The following code will be included if the source file is a "*.c" file. */
