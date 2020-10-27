791ed55d42c3ad74358d65845fcce7811b4a6c31
tracer-ics2016 <tracer@njuics.org>
2020-10-27 13:36:16 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:36:16 up 3 days, 45 min,  1 user,  load average: 1.63, 1.48, 1.40 ffea1d5260478be53746bc502e1c88cca571f6ba

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -2,18 +2,31 @@
 
 #define instr jmp
 
-static void do_execute() {
-	cpu.eip += op_src->val;
-	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
+make_helper(concat(jmp_i_, SUFFIX)) {
+	int len = concat(decode_i_, SUFFIX)(eip + 1);
+	cpu.eip += (DATA_TYPE_S)op_src->val;
+	print_asm("jmp 0x%x", cpu.eip + len + 1);
+	return len + 1;
 }
 
-make_instr_helper(si)
-#if DATA_BYTE == 4
-make_helper(jmp_rm_l) {
-	int len = decode_rm_l(eip + 1);
-	cpu.eip = op_src->val - (len + 1);
-	print_asm(str(instr) " *%s", op_src->str);
+make_helper(concat(jmp_rm_, SUFFIX)) {
+	int len = concat(decode_rm_, SUFFIX)(eip + 1);
+	cpu.eip = (DATA_TYPE_S)op_src->val - len - 1;
+	print_asm("jmp %s", op_src->str);
 	return len + 1;
+}
+
+#if DATA_BYTE == 4
+make_helper(ljmp) {
+	uint32_t op1 = instr_fetch(eip + 1, 4) - 7;
+	uint16_t op2 = instr_fetch(eip + 5, 2);
+	cpu.eip = op1;		//modify eip
+	cpu.cs.val = op2;	
+
+	loadSregCache(R_CS);
+
+	print_asm("ljmp %x,0x%x", op2, op1 + 7);
+	return 7;
 }
 #endif
 #include "cpu/exec/template-end.h"
